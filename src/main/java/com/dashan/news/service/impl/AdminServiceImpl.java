3a5d245a252035bde9517ba4dec472dd524767d6
shanchun02 <1446533395@qq.com>
2019-09-26 17:14:08 +0800

    大小事新闻网SpringBoot 0.1

---
+++ src/main/java/com/dashan/news/service/impl/AdminServiceImpl.java
@@ -0,0 +1,10 @@
+package com.dashan.news.service.impl;
+
+/**
+ * @ClassName: AdminServiceImpl
+ * @Description: 后台管理员操作类
+ * @author: shanchun
+ * @date: 2019/09/26  下午 5:11
+ */
+public class AdminServiceImpl {
+}
