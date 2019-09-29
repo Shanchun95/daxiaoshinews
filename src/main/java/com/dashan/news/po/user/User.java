e1660557bfedb75fbc7a896f5cd2363c5a333c5b
shanchun02 <1446533395@qq.com>
2019-09-29 09:48:40 +0800

    pom文件更新

---
+++ src/main/java/com/dashan/news/po/user/User.java
@@ -0,0 +1,113 @@
+package com.dashan.news.po.user;
+
+public class User {
+    private Integer userId;
+
+    private String userName;
+
+    private String password;
+
+    private String name;
+
+    private String sex;
+
+    private String age;
+
+    private String phone;
+
+    private String address;
+
+    private String remark;
+
+    private Integer createtime;
+
+    private Integer updatetime;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age == null ? null : age.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public Integer getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Integer createtime) {
+        this.createtime = createtime;
+    }
+
+    public Integer getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Integer updatetime) {
+        this.updatetime = updatetime;
+    }
+}
